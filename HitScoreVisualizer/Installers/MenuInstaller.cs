86c5f22dacfcb9d42f4ac2dce9f85cbf0cead3ee
Eris <erisapps@outlook.be>
2020-10-14 01:51:55 +0200

    Updated to SiraUtil 2.0.0

--- HitScoreVisualizer/Installers/MenuInstaller.cs
+++ HitScoreVisualizer/Installers/MenuInstaller.cs
@@ -1,22 +1,18 @@
-﻿using BeatSaberMarkupLanguage;
-using HitScoreVisualizer.UI;
-using SiraUtil.Zenject;
+﻿using HitScoreVisualizer.UI;
+using SiraUtil;
 using Zenject;
+using Utilities = SiraUtil.Utilities;
 
 namespace HitScoreVisualizer.Installers
 {
-	[RequiresInstaller(typeof(AppInstaller))]
-	public class MenuInstaller : MonoInstaller
+	public class MenuInstaller : Installer<MenuInstaller>
 	{
 		public override void InstallBindings()
 		{
 			Plugin.LoggerInstance.Debug($"Running {nameof(InstallBindings)} of {nameof(MenuInstaller)}");
 
-			var configSelectorViewController = BeatSaberUI.CreateViewController<ConfigSelectorViewController>();
-			var hitScoreFlowCoordinator = BeatSaberUI.CreateFlowCoordinator<HitScoreFlowCoordinator>();
-
-			Container.InjectSpecialInstance<ConfigSelectorViewController>(configSelectorViewController);
-			Container.InjectSpecialInstance<HitScoreFlowCoordinator>(hitScoreFlowCoordinator);
+			Container.Bind<ConfigSelectorViewController>().FromNewComponentOnNewGameObject().AsSingle().OnInstantiated(Utilities.SetupViewController);
+			Container.Bind<HitScoreFlowCoordinator>().FromNewComponentOnNewGameObject().AsSingle();
 
 			Plugin.LoggerInstance.Debug($"Binding {nameof(SettingsControllerManager)}");
 			Container.BindInterfacesAndSelfTo<SettingsControllerManager>().AsSingle().NonLazy();
